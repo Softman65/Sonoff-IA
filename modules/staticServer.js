f6421576cc3e324b28ee2a65903be87a011ce7e7
Ricardo Benitez <rbenitez2012@gmail.com>
2020-10-25 06:04:28 +0100

    .

--- modules/staticServer.js
+++ modules/staticServer.js
@@ -12,11 +12,14 @@ module.exports = function (app) {
                     res.statusCode = 200;
 
                     res.write(app.ejs.render(data.toString(), {
-                        Sidebar: [
-                          { href: "pages/calendar.html", p: "Calendar", class: "nav-icon fas fa-calendar-alt" },
-                          { href: "pages/gallery.html", p: "Gallery", class: "nav-icon far fa-image" },
-                          { href: "pages/kanban.html", p: "Kanban Board", class: "nav-icon fas fa-columns" }
-                        ]
+                        Sidebar: {
+                            header:"EXAMPLES",
+                            li: [
+                                { href: "pages/calendar.html", p: "Calendar", class: "nav-icon fas fa-calendar-alt" },
+                                { href: "pages/gallery.html", p: "Gallery", class: "nav-icon far fa-image" },
+                                { href: "pages/kanban.html", p: "Kanban Board", class: "nav-icon fas fa-columns" }
+                            ]
+                        }
                     }, { root: resolvedBase }) );
                     return res.end();
                 })
