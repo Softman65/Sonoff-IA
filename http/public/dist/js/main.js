7b3140b8fdaed137e9b4864d775cc69b32acd87f
Ricardo Benitez <rbenitez2012@gmail.com>
2020-10-25 06:15:42 +0100

    .

--- http/public/dist/js/main.js
+++ http/public/dist/js/main.js
@@ -8,3 +8,19 @@
         console.log(data);
         //socket.emit('my other event', {my: 'data' });
     });
+
+
+function Menu_sel(hash) {
+    alert(hash)
+}
+
+function locationHashChanged(e) {
+    console.log(location.hash);
+    console.log(e.oldURL, e.newURL);
+    Manu_sel(location.hash)
+    //if (location.hash === "#pageX") {
+    //    pageX();
+    //}
+}
+
+window.onhashchange = locationHashChanged;
